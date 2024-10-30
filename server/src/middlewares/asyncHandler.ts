ba927093ec2160fc6e207ebe54e4933b04168e78
Julia Noczyńska <noczynska.julia@gmail.com>
2024-10-30 19:13:13 +0100

    chore: delete asyncHandler middleware, as it is no longer used

--- server/src/middlewares/asyncHandler.ts
+++
@@ -1,5 +0,0 @@
-import {NextFunction, Request, Response} from "express";
-
-export const asyncHandler = (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-    return Promise.resolve(fn(req, res, next)).catch(next);
-};
